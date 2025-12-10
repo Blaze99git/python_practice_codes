6c700f807f671097cde21d8614bcb49156fe6075
Utkarsh <60348194+Blaze99git@users.noreply.github.com>
2025-12-10 23:23:17 +0530

    Fix docstring grammar in multiply function
    Corrected grammar in the docstring regarding string multiplication.

--- Day3/functions/3rd_solution_Polymorph.py
+++ Day3/functions/3rd_solution_Polymorph.py
@@ -1,6 +1,6 @@
 '''3. Polymorphism in Functions
 Problem: Write a function multiply that multiplies two numbers, 
-but can also accept and multiply strings.
+but can also accept and multiply string.
 '''
 def multiply(a, b):
     return a * b

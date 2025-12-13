c19a8f26d65efa0061e5ce90645387a8c58ad201
Utkarsh <60348194+Blaze99git@users.noreply.github.com>
2025-12-13 21:53:48 +0530

    Delete Day3 directory

--- Day3/loops/4th_solution_reversestring.py
+++
@@ -1,9 +0,0 @@
-'''
-4. Reverse a String
-Problem: Reverse a string using a loop.
-'''
-string =input("String to reverse using a loop: ")
-reversed_string = ""
-for char in string:
-    reversed_string = char + reversed_string
-print("Reversed string:", reversed_string)
